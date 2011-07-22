57f978b91d6220882797bee1a5bb6d030381c907
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2011-07-22 16:42:52 -0300

    Phobos: doc for the ResponseHandler class

--- qtclient/phobos/responsehandler.h
+++ qtclient/phobos/responsehandler.h
@@ -56,49 +56,93 @@ struct PHOBOSRPC_EXPORT Error
 class PHOBOSRPC_EXPORT ResponseHandler
 {
 public:
+    /*!
+      Creates a ResponseHandler that will use \param peer
+      when responding some message.
+      */
     explicit ResponseHandler(Peer *peer = 0);
 
+    /*! method getter.
+      @return a string containing the method name
+      */
     QString method() const;
+    /*! Sets the method.
+      \param method can be any valid name according
+      the json-rpc 2.0 spec.
+      e.g.: not starting with "rpc."
+      @return true if is a valid method
+      */
     bool setMethod(const QString &method);
 
+    /*!
+      @return true if params map/array exists
+      @sa resetParams
+      */
     bool hasParams() const;
+    /*! params getter
+      @return the params object (a QVariantMap or a QVariantList),
+      or a null QVariant if params object doesn't exist.
+      @sa hasParams
+      @sa setParams
+      */
     QVariant params() const;
+    /*! params setter
+      \param params can be any valid params object according
+      the json-rpc 2.0 spec (QVariantMap and QVariantList).
+      @return true if is a valid params object.
+      @sa params
+      */
     bool setParams(const QVariant &params);
+    /*! Remove the params object.
+      @sa hasParams
+      @sa setParams
+      */
     void resetParams();
 
     /*!
-      @return true if the response has an id
+      @return true if the response has an id.
+      @sa resetId
       */
     bool hasId() const;
     /*!
-      @return the id, or a null QVariant if id doesn't exist
+      If id doesn't exists, then a response message won't be
+      generated when you use the response or error methods.
+      @return the id, or a null QVariant if id doesn't exist.
+      @sa hasId
+      @sa setId
       */
     QVariant id() const;
-    /*! validates and sets the id
-      this id will be used in the JSON-RPC response message object
-      @return returns true if \param id is a valid id
-      @sa hasId
+    /*! Validates and sets the id.
+      This id will be used in the JSON-RPC response message object.
+      @return true if \param id is a valid id.
+      @sa id
       */
     bool setId(const QVariant &id);
-    /*! removes any id associated with the response object
+    /*! Removes any id associated with the response object.
+      @sa hasId
       @sa setId
       */
     void resetId();
 
-    /*!
-      @return false if you can send a reply to the peer object
+    /*! A ResponseHandler object is null if you can't send a message.
+      This happens when you already sent a message (either using response
+      or error methods) or when you constructed the ResponseHandler with
+      no peer
+      @return false if you can send a reply to the peer object.
       */
     bool isNull() const;
-    /*! sends the response object to the peer object, if it still exists
-      * use this method when you wants send the response.
-      * use this method will put the object in the null state.
-      * use this method when the object is in null state won't do anything
+    /*! Sends the response object to the peer object, if it still exists.
+      Use this method when you wants send the response.
+      @warning use this method when the object is in null state won't do
+      anything
+      @sa isNull
       */
     void response(const QVariant &result);
-    /*! sends the response error object to the peer object, if it still exists
-      * use this method when you wants send an error response.
-      * use this method will put the object in the null state.
-      * use this method when the object is in null state won't do anything
+    /*! Sends the response error object to the peer object, if it still exists.
+      Use this method when you wants send an error response.
+      @warning use this method when the object is in null state won't do
+      anything
+      @sa isNull
       */
     void error(const Error &error);
 
